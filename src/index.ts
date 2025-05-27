bc2ea730778d314375b12f3cae473debe24b8009
Dariusz Kanicki <github@dariusz.kanicki.net>
2025-05-27 21:37:19 +0200

    progress

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 import type { App, Plugin } from 'obsidian';
 import { Renderer } from './renderer/renderer.js';
 import { SettingsConfig } from './renderer/types-api.js';
-import { injectPrefixedStyles } from './utils/inject-styles.js';
 
 // 🔹 Entrypoints
 
@@ -19,5 +18,3 @@ export async function renderSettings<T>(
   const renderer = new Renderer(app, plugin, config, settings, defaults, container, saveData, refreshSettings);
   return renderer.renderSettings();
 }
-
-export { injectPrefixedStyles };
